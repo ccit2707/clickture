287300658dc6d65d86789b3eb3fd495658ae7db9
ccit2707 <saad.a.rabbani@gmail.com>
2015-08-04 18:55:50 -0400

    Clickture Full theme Upload
    Major Changes include:
    function.php: lines 89-160
    content-locations.php - located in template-parts
    Delete options.php - as i am not done with it yet

---
+++ functions.php
@@ -0,0 +1,232 @@
+<?php
+/**
+ * clickture functions and definitions
+ *
+ * @package clickture
+ */
+
+if ( ! function_exists( 'clickture_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function clickture_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on clickture, use a find and replace
+	 * to change 'clickture' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'clickture', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => esc_html__( 'Primary Menu', 'clickture' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+	) );
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'clickture_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+}
+endif; // clickture_setup
+add_action( 'after_setup_theme', 'clickture_setup' );
+
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+ 
+// SAAD BEGIN CUSTOM POST TYPE	
+function custom_post_type() {
+
+// Set UI labels for Custom Post Type
+	$labels = array(
+		'name'                => _x( 'Locations', 'Post Type General Name', 'clickture' ),
+		'singular_name'       => _x( 'Location', 'Post Type Singular Name', 'clickture' ),
+		'menu_name'           => __( 'Locations', 'clickture' ),
+		'all_items'           => __( 'All Locations', 'clickture' ),
+		'view_item'           => __( 'View Location', 'clickture' ),
+		'add_new_item'        => __( 'Add New Location', 'clickture' ),
+		'add_new'             => __( 'Add New Location', 'clickture' ),
+		'edit_item'           => __( 'Edit Location', 'clickture' ),
+		'update_item'         => __( 'Update Location', 'clickture' ),
+		'search_items'        => __( 'Search Location', 'clickture' ),
+		'not_found'           => __( 'Not Found', 'clickture' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'clickture' ),
+	);
+	
+// Set other options for Custom Post Type
+	
+	$args = array(
+		'label'               => __( 'locations', 'clickture' ),
+		'description'         => __( 'Interesting Locations in the GTA', 'clickture' ),
+		'labels'              => $labels,
+		// Features this CPT supports in Post Editor
+		'supports'            => array( 'title', 'editor', 'excerpt', 'thumbnail', 'comments', ),
+		// You can associate this CPT with a taxonomy or custom taxonomy. 
+		'taxonomies'          => array( 'category', 'post_tag', 'locations'),
+		/* A hierarchical CPT is like Pages and can have
+		* Parent and child items. A non-hierarchical CPT
+		* is like Posts.
+		*/	
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	
+	// Registering your Custom Post Type
+	register_post_type( 'locations', $args );
+
+}
+
+/* Hook into the 'init' action so that the function
+* Containing our post type registration is not 
+* unnecessarily executed. 
+*/
+
+add_action( 'init', 'custom_post_type', 0 );
+
+
+
+function add_my_post_types_to_query( $query ) {
+	if ( is_home() && $query->is_main_query() )
+		$query->set( 'post_type', array( 'post', 'locations' ) );
+	return $query;
+}
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' ); 
+// the code is fine if the links to Custom posts are not working change and reset the permalinks settings - under the settings menu in the WP tool bar 
+
+
+require get_stylesheet_directory() . '/inc/options.php';
+// FINISH CUSTOM POST TYPE
+
+
+
+
+
+
+
+
+ 
+function clickture_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'clickture_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'clickture_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_sidebar
+ */
+function clickture_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'clickture' ),
+		'id'            => 'sidebar-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'clickture_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function clickture_scripts() {
+	wp_enqueue_style( 'clickture-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'clickture-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+	wp_enqueue_script( 'clickture-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'clickture_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/jetpack.php';
