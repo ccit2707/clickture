ab7ce475f6e4f6b546f76edbbe62fc95c8600948
kylestyle92 <fan_of_1978@hotmail.com>
2015-08-07 01:04:43 -0400

    Final Final Changes

--- template-parts/page_custom.php
+++ template-parts/page_custom.php
@@ -12,12 +12,13 @@
 		
 	<div id="gridcontainer">
 		<?php
+			
 			$counter = 1;
 			$grid = 2;	
 			 
 			$paged = ( get_query_var('page') ) ? get_query_var('page') : 1; 
 
-			$args = array('post_type' => 'locations', 'showposts' => 2, 'order' => 'ASC', 'paged' => $paged );
+			$args = array('post_type' => 'locations', 'showposts' => 4, 'order' => 'ASC', 'paged' => $paged );
 			$my_query = new WP_Query($args);
 		?>		
 		
@@ -25,38 +26,38 @@
 	if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post();
 	
 
-        if ($counter == 1)
-		{
+        if($counter == 1) :
+		
 	?>		<div class="gridleft">
-			<h3> <a href=" <?php the_permalink() ?> "> <?php the_title(); ?> </a></h3>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
 			</div>
 	<?php	
-		}
-        else if ($counter == $grid)
-		{
+		
+        elseif($counter == $grid) :
+		
 	?>		
-			<div class="gridleft">
-			<h2> <a href=" <?php the_permalink() ?> "> <?php the_title(); ?> </a> </h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+			<div class="gridright">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
 			</div>
 	<?php
-		}
+		
 
+		$counter = 0;
 		
+		endif;
 		
-		
-		
-		$counter == 1;
+		$counter ++;
 		
 		endwhile;
 		
 		endif;
-		
-		echo get_next_posts_link( 'Older Entries', $my_query->max_num_pages );
-		echo get_previous_posts_link( 'Newer Entries' );
-	
 	?>
+		<div id="navigate_btn"><?php
+			echo get_next_posts_link( 'Older Entries', $my_query->max_num_pages );
+			echo get_previous_posts_link( 'Newer Entries' );
+	?>
+		</div>
+		
 	</div>
 	
 	<div id="customfooter"> <?php get_footer(); ?> </div>
